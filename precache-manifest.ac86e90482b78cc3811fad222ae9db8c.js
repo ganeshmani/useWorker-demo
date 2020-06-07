4b30cbf13170ff3ea519497f89cdcd23ca098c4f
mac <mac@macs-MacBook-Pro.local>
2020-06-07 18:39:06 +0530

    Updates

---
+++ precache-manifest.ac86e90482b78cc3811fad222ae9db8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44807fb8ad117895cd95ae008f8981d",
+    "url": "/useWorker-demo/index.html"
+  },
+  {
+    "revision": "7638e30a538e7dbb9cb9",
+    "url": "/useWorker-demo/static/css/main.ef96ee22.chunk.css"
+  },
+  {
+    "revision": "d9340c2933f303586328",
+    "url": "/useWorker-demo/static/js/2.12810801.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/useWorker-demo/static/js/2.12810801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7638e30a538e7dbb9cb9",
+    "url": "/useWorker-demo/static/js/main.21126294.chunk.js"
+  },
+  {
+    "revision": "daeeaeafecf35cada4c5",
+    "url": "/useWorker-demo/static/js/runtime-main.3ff064bd.js"
+  }
+]);
